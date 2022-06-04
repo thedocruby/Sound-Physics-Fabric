e86b3b746d972cdee2e5f3e3a91485cc43d1f65a
Mike Nrafter <mikenrafter@protonmail.com>
2022-06-04 00:17:37 -0600

    renamed ModClient/Server.java

--- src/main/java/dev/thedocruby/resounding/ModClient.java
+++ src/main/java/dev/thedocruby/resounding/ModClient.java
@@ -4,7 +4,7 @@ import dev.thedocruby.resounding.config.BlueTapePack.ConfigManager;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.EnvType;
 
-public class ResoundingModClient implements ClientModInitializer {
+public class ModClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
         Engine.env = EnvType.CLIENT;
--- src/main/java/dev/thedocruby/resounding/ModServer.java
+++ src/main/java/dev/thedocruby/resounding/ModServer.java
@@ -4,7 +4,7 @@ import dev.thedocruby.resounding.config.BlueTapePack.ConfigManager;
 import net.fabricmc.api.DedicatedServerModInitializer;
 import net.fabricmc.api.EnvType;
 
-public class ResoundingModServer implements DedicatedServerModInitializer {
+public class ModServer implements DedicatedServerModInitializer {
     @Override
     public void onInitializeServer() {
         Engine.env = EnvType.SERVER;
