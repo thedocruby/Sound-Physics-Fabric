fc60ff429820d5b524216d773427fdb699221837
Mike Nrafter <mikenrafter@protonmail.com>
2022-06-04 00:05:34 -0600

    save before refactor

--- src/main/java/dev/thedocruby/resounding/Utils.java
+++ src/main/java/dev/thedocruby/resounding/Utils.java
@@ -1,1 +1,31 @@
 package dev.thedocruby.resounding;
+
+import org.apache.commons.lang3.ArrayUtils;
+
+/**
+* Utils
+*/
+public class Utils {
+
+// java's type system sucks... overloading, ugh - even python could do better...
+// an overloaded array length extender function boolean[], boolean[][], int[], int[][] {
+
+public static int[]       extendArray (final int[]       old, final int min) {
+	return ArrayUtils.addAll(old, new int[Math.max(1,old.length - min)]);
+}
+
+public static int[][]     extendArray (final int[][]     old, final int min) {
+	return ArrayUtils.addAll(old, new int[Math.max(1,old.length - min)][]);
+}
+
+public static boolean[]   extendArray (final boolean[]   old, final int min) {
+	return ArrayUtils.addAll(old, new boolean[Math.max(1,old.length - min)]);
+}
+
+public static boolean[][] extendArray (final boolean[][] old, final int min) {
+	return ArrayUtils.addAll(old, new boolean[Math.max(1,old.length - min)][]);
+}
+
+// }
+
+}
--- src/main/java/dev/thedocruby/resounding/openal/EFXContext.java
+++ src/main/java/dev/thedocruby/resounding/openal/EFXContext.java
@@ -8,23 +8,65 @@ import org.lwjgl.openal.AL10;
 import org.lwjgl.openal.AL11;
 import org.lwjgl.openal.ALC10;
 import org.lwjgl.openal.EXTEfx;
+import org.lwjgl.openal.EXTThreadLocalContext;
+
 import net.minecraft.util.math.MathHelper;
 
 import static dev.thedocruby.resounding.config.PrecomputedConfig.pC;
 
+import javax.annotation.Nullable;
+
+
 @Environment(EnvType.CLIENT)
 public class EFXContext { // TODO: Create separate debug toggle for OpenAl EFX instead of using pC.dLog
 
-	// allow inheritance of settings via children contexts
-	public static EFXContext[] children = new EFXContext[0];
+	// class containing AL context information
+	private class ALContext {
+		private ALContext() {}
 
-	// empty arrays
-    private static int[]  slots        = new int[0];
-    private static int[]  effects      = new int[0];
-    private static int[]  filters      = new int[0];
+		// AL objects, intentionally non-static
+		public long   old     = -1        ; // context id
+		public long   self    = -1        ; // context id
+		public int    direct  = -1        ; // directFilter
+		public int[]  slots   = new int[0];
+		public int[]  effects = new int[0];
+		public int[]  filters = new int[0];
+	}
+
 	// default values
-    private static int    directFilter = 0;
-    public static boolean initialized = false;
-    public static boolean efxEnabled  = true;
+	private static ALContext context        ;
+	public  static boolean   active  = false;
+	public  static boolean   enabled = true ;
+
+	// optional values
+	@Nullable public static String id;
+	// main interface
+	// allow inheritance of settings via children contexts
+	public         EFXContext[] children = new EFXContext[0];
+	// pipeline
+	public  static Effect[] effects = new Effect[0];
+
+	// context control {
+	public static boolean activate() {
+		context.old = EXTThreadLocalContext.alcGetThreadContext();
+		EXTThreadLocalContext.alcSetThreadContext(context.self);
+		return !ALUtils.checkErrors("Error while activating openAL context "+context.self+".");
+	}
+	public static boolean deactivate() {
+		EXTThreadLocalContext.alcSetThreadContext(context.old);
+		return !ALUtils.checkErrors("Error while reactivating openAL context "+context.old+".");
+	}
+	// }
+
+	public static boolean setup() {
+		if (enabled || !active || context.direct > 0) return true; // already setup?
+		initSlots  ();
+		initEffects();
+		initFilters();
+		ResoundingEngine.LOGGER.info("New context: {}.", id == null ? "<unnamed>" : id);
+
+		return false;
+	}
+	public boolean clean() {return false;}
 
 }
