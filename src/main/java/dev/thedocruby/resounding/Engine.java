ca90b2cfd84168f4511005070de6918e1b4b3d5c
Jack Gabriel C. Acodili <112671361+acodili-jg@users.noreply.github.com>
2024-07-01 19:15:21 +0800

    Get simulation distance from options
     - the field is now private and was replaced with a getter
     - the getter returns a `net.minecraft.client.option.SimpleOption<Integer>`
     - the value can be retrieved by calling `SimpleOption#getValue()`

--- src/main/java/dev/thedocruby/resounding/Engine.java
+++ src/main/java/dev/thedocruby/resounding/Engine.java
@@ -136,7 +136,7 @@ public class Engine {
 		listenerPos = lastSoundListener.getTransform().position();
 		double maxDist = Math.min(
 				Math.min(
-						Math.min(mc.options.simulationDistance, mc.options.viewDistance),
+						Math.min(mc.options.getSimulationDistance().getValue(), mc.options.viewDistance),
 						pC.soundSimulationDistance
 				) * 16, // chunk
 				pC.maxTraceDist / 2); // diameter -> radius
