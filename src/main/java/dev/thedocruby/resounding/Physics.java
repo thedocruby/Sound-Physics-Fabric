cd05bb0d2839efdd2d22d5af1af915df8fc7fef8
Jack Gabriel Acodili <112671361+acodili-jg@users.noreply.github.com>
2024-07-12 21:28:20 +0800

    Hardcode return zero on equal impedance
    This expects that the parameters are changed to primitive `double` as the same equality check won't work on boxed type. A reflectivity of zero is now returned when both impedance are zero instead of NaN.

--- src/main/java/dev/thedocruby/resounding/Physics.java
+++ src/main/java/dev/thedocruby/resounding/Physics.java
@@ -36,6 +36,6 @@ public class Physics {
 
     public static @NotNull Double reflection(@NotNull Double impedanceA, @NotNull Double impedanceB) {
         // difference of squares
-        return Math.pow( (impedanceA - impedanceB) / (impedanceA + impedanceB), 2);
+        return impedanceA == impedanceB ? 0.0 : Math.pow( (impedanceA - impedanceB) / (impedanceA + impedanceB), 2);
     }
 }
