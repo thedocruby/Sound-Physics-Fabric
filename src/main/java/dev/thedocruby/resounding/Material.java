d2fd1f1a656188a1e3584497b457ebd278b5a51f
Jack Gabriel Acodili <112671361+acodili-jg@users.noreply.github.com>
2024-07-13 15:36:04 +0800

    Update Material.java (#113)
    * Update Material.java
    
     - Replaced @NotNull Double with a primitive double
     - Input validation for component state to be between zero and one
     - Documentation
    
    * Material: remove explicit accessors for impedance and permeation
    
    * Material: readd mention permeation is the inverse of absorption
    
    * Material fallback values

--- src/main/java/dev/thedocruby/resounding/Material.java
+++ src/main/java/dev/thedocruby/resounding/Material.java
@@ -1,10 +1,53 @@
 package dev.thedocruby.resounding;
 
-import org.jetbrains.annotations.NotNull;
+/**
+ * A material.
+ *
+ * @param impedance  the impedance of the material
+ * @param permeation  the permeation of the material (the inverse of absorption)
+ * @param state  the current state of matter
+ */
+public record Material(double impedance, double permeation, double state) {
+    /**
+     * The fallback material.
+     */
+    public static final Material FALLBACK = new Material(350.0, 1.0, 1.0);
 
-public record Material(
-        @NotNull Double impedance,   // impedance of material
-        @NotNull Double permeation,  // permeation of material (inverse of absorption)
-        @NotNull Double state        // state of matter [0 absent, .25 plasma, .5 gas, .75 liquid, 1 solid]
-) {
+    /**
+     * Creates a new material.
+     * <p>
+     * For valid values for the state of matter, see {@code state()}.
+     *
+     * @param impedance  the impedance of the material
+     * @param permeation  the permeation of the material
+     * @param state  the current state of matter
+     * @see #state()
+     */
+    public Material {
+        if (state < 0.0 || state > 1.0) {
+            throw new IllegalArgumentException(
+                    "Expected component state to be between zero and one but instead its " + state
+            );
+        }
+    }
+
+    /**
+     * The current state of matter. This is a continuous value in the range
+     * between zero and one, with each state roughly mapped in the following
+     * table:
+     * <table>
+     *     <tr><th>Value</th> <th>State of Matter</th></tr>
+     *     <tr><td>0    </td> <td>absent         </td></tr>
+     *     <tr><td>0.25 </td> <td>plasma         </td></tr>
+     *     <tr><td>0.5  </td> <td>gas            </td></tr>
+     *     <tr><td>0.75 </td> <td>liquid         </td></tr>
+     *     <tr><td>1.0  </td> <td>solid          </td></tr>
+     * </table>
+     *
+     * @return the current state of matter
+     */
+    @Override
+    public double state() {
+        return this.state;
+    }
 }
